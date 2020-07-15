95a4d33393fc3ba87d05bc40083aff075418b0bd
xiaofandegeng <434962239@qq.com>
2020-07-15 20:21:59 +0800

    重构代码结构

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
@@ -12,12 +12,10 @@ public class PaymentServiceImpl implements PaymentService{
     @Resource
     private PaymentDao paymentDao;
 
-    @Override
     public int create(Payment payment) {
         return paymentDao.create(payment);
     }
 
-    @Override
     public Payment getPaymentById(Long id) {
         return paymentDao.getPaymentById(id);
     }
