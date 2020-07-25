216d34fae598fcb1af12606efd0a0d818ecf59ee
xiaofandegeng <434962239@qq.com>
2020-07-25 11:41:19 +0800

    修改负载均衡计算策略

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OderMain80.java
@@ -1,11 +1,14 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.irule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PROVIDER-SERVICE", configuration = MyRule.class)
 public class OderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OderMain80.class, args);
