e7afefd38b3389c63e280d6834cc393aa8adf9fe
xiaofandegeng <434962239@qq.com>
2020-08-11 23:32:42 +0800

    初步完成sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/ning4256/controller/FlowLimitController.java
@@ -0,0 +1,21 @@
+package com.ning4256.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA(){
+        return "---testA---";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "---testB---";
+    }
+
+}
