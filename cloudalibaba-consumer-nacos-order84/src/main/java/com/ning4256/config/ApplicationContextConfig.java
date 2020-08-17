77a089463772ceaf5de3ce28ea290e3df88bd8e3
xiaofandegeng <434962239@qq.com>
2020-08-17 20:37:52 +0800

    ribbon系列

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/ning4256/config/ApplicationContextConfig.java
@@ -0,0 +1,17 @@
+package com.ning4256.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextConfig {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
